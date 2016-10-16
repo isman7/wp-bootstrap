1cd10d9313d14ec9082779ec50252d88a694c67c
ismansiete <ismansiete@isman-desktop>
2016-10-16 14:08:54 +0200

    Modifying header to allow Fisitronica logo.

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title><?php bloginfo('name'); ?> | <?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
- 
+
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
@@ -19,16 +19,16 @@
 <link href='http://fonts.googleapis.com/css?family=Buenard:400,700' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
- 
+
 <?php
-    /* 
+    /*
      *  Add this to support sites with sites with threaded comments enabled.
      */
     if ( is_singular() && get_option( 'thread_comments' ) )
         wp_enqueue_script( 'comment-reply' );
 
     wp_get_archives('type=monthly&format=link');
- 
+
     wp_head();
 ?>
 </head>
@@ -38,36 +38,44 @@
 
         <div class="navbar navbar-inverse navbar-static-top" role="navigation">
           <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <?php if ( get_theme_mod( 'themeslug_logo' ) ) : ?>
-        		<a class="navbar-brand" href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
-			  <?php else : ?>
-        			<a class="navbar-brand" href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a>
-			  <?php endif; ?>
+            <div class="row">
+              <div class="col-md-2"
+                  <?php if ( get_theme_mod( 'themeslug_logo' ) ) : ?>
+                      <a class="navbar-brand" href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'>
+                        <img class="img-responsive" src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
+                      </a>
+                  <?php else : ?>
+                        <a class="navbar-brand" href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a>
+                  <?php endif; ?>
+              </div>
+
+              <div class="col-md-10">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+              </div>
+              <div class="navbar-collapse collapse">
+           	<?php  /* menu */
+           	wp_nav_menu( array(
+          		'menu'              => 'primary',
+          		'theme_location'    => 'primary-menu',
+          		'depth'             => 2,
+          		'container'         => 'div',
+          		'container_class'   => 'collapse navbar-collapse navbar-ex1-collapse',
+          		'menu_class'        => 'nav navbar-nav',
+          		'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+          		'walker'            => new wp_bootstrap_navwalker())
+      		);
+  			?>
+              </div>
             </div>
-            <div class="navbar-collapse collapse">
-         	<?php  /* menu */
-         	wp_nav_menu( array(
-        		'menu'              => 'primary',
-        		'theme_location'    => 'primary-menu',
-        		'depth'             => 2,
-        		'container'         => 'div',
-        		'container_class'   => 'collapse navbar-collapse navbar-ex1-collapse',
-        		'menu_class'        => 'nav navbar-nav',
-        		'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-        		'walker'            => new wp_bootstrap_navwalker())
-    		); 
-			?>
             </div>
           </div>
         </div>
 
       </div>
     </div>
-    
